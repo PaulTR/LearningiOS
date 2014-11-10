04db25dcd09e2459ac5fe3c8105eec2064387c0c
Paul Ruiz <paultr@sportslabs.com>
2014-11-09 22:00:24 -0700

    chapter 2 of beginning ios 7 development done

---
+++ Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Paul Ruiz on 11/9/14.
+//  Copyright (c) 2014 PTRPrograms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
