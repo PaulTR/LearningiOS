dbdcd2196b6c38d653ecc74d6ffae43791872a79
Paul Trebilcox-Ruiz <paultr@gmail.com>
2015-08-18 11:04:36 -0600

    settings screens and default values via bridge control example app

--- swift/Bridge Control/Bridge Control/SecondViewController.swift
+++ swift/Bridge Control/Bridge Control/SecondViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var engineSwitch:UISwitch!
+    @IBOutlet var warpFactorSlider:UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshFields()
+        
+        
+        let app = UIApplication.sharedApplication()
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "applicationWillEnterForeground:",
+            name: UIApplicationWillEnterForegroundNotification,
+            object: app)
+    }
+    
+    func refreshFields() {
+            let defaults = NSUserDefaults.standardUserDefaults()
+            engineSwitch.on = defaults.boolForKey(warpDriveKey)
+            warpFactorSlider.value = defaults.floatForKey(warpFactorKey)
+    }
+    
+    func applicationWillEnterForeground(notification:NSNotification) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.synchronize()
+        refreshFields()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,27 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func engineSwitchTapped(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setBool(engineSwitch.on, forKey: warpDriveKey)
+        defaults.synchronize()
+    }
+    
+    @IBAction func warpSliderTouched(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(warpFactorSlider.value, forKey: warpFactorKey)
+        defaults.synchronize()
+    }
+    
+    @IBAction func settingsButtonClicked(sender: AnyObject) {
+            UIApplication.sharedApplication().openURL(
+            NSURL(string: UIApplicationOpenSettingsURLString)!)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+            super.viewDidDisappear(animated)
+            NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
 
 }
 
---
+++ swift/Bridge Control/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Bridge Control
+//
+//  Created by Paul Trebilcox on 8/18/15.
+//  Copyright (c) 2015 ptrprograms. All rights reserved.
+//
+
+import Foundation
+
+let officerKey = "officer"
+let authorizationCodeKey = "authorizationCode"
+let rankKey = "rank"
+let warpDriveKey = "warp"
+let warpFactorKey = "warpFactor"
+let favoriteTeaKey = "favoriteTea"
+let favoriteCaptainKey = "favoriteCaptain"
+let favoriteGadgetKey = "favoriteGadget"
+let favoriteAlienKey = "favoriteAlien"
